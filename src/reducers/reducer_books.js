fe3c9b0d134c87830ef6456810c1c68c9913845f
Mario Zamora <maza1387@gmail.com>
2017-06-22 16:03:53 -0700

    Complete App with REDUX implementaion

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    {title: 'The Alchemist', pages: 167},
+    {title: 'Average Skill, Phenomenal Will', pages: 89},
+    {title: 'The Keys', pages: 212},
+    {title: 'Mastery', pages: 316}
+  ]
+}
